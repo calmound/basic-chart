6b262d6d481148e1f91cd7508ab8b98f01da92ce
zhangchengbing <zhangchengbing@oschina.cn>
2022-01-05 18:01:00 +0800

    fix:count图表修改cssModule

--- src/components/basic-count-chart/View.tsx
+++ src/components/basic-count-chart/View.tsx
@@ -6,7 +6,7 @@ import { NoData } from 'proxima-sdk/components/Components/Chart';
 
 import { ViewProps } from '../lib/type';
 import classNames from 'classnames';
-import './View.less';
+import cx from './View.less';
 /**
  * todo
  * 接口调试之后略微修改
@@ -27,8 +27,8 @@ const View: React.FC<ViewProps> = ({ random, option, tenant, sessionToken, isLis
     <>
     {!data ? <NoData title="暂无数据，请修改图表数据配置" isListView={isListView} /> 
     : 
-    <div id={id} className={classNames(isListView ? 'basic-chart-count-list' : 'basic-chart-count-detail')} >
-      <span className={classNames('view-filter-data')}><span>34.0</span><span style={{ float: option?.unit }}>{option?.unitName}</span></span>
+    <div id={id} className={cx(isListView ? 'basic-chart-count-list' : 'basic-chart-count-detail')} >
+      <span className={cx('view-filter-data')}><span>34.0</span><span style={{ float: option?.unit }}>{option?.unitName}</span></span>
     </div>}
     
   </>
