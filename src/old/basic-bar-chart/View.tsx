c317fe8ba92b96189bf96391a6ab15fe254924fe
zhusen <zhusen@oschina.cn>
2022-01-07 10:57:31 +0800

    chore:删除多余文件

--- src/old/basic-bar-chart/View.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Bar = () => {
-  return <div>bar
-      <div id="basic-pie-chart" style={{ width: '100%', height: '300px' }}>1</div>
-  </div>;
-};
-
-export default Bar;
